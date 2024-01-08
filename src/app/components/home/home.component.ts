48806fbd1561be81a43bb0337d53b21d81bc0590
jesternook <nicocossta@outlook.com>
2024-01-08 17:17:28 -0400

    alterações iniciais na tela home
    - alterações na tela home
    - alterações na navbar
    - alterações de guardas e rotas da pagina home e welcome

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,10 +1,21 @@
 import { Component } from '@angular/core';
 
+export interface Card {
+  picture: string;
+  name: string;
+  age: number;
+  description: string;
+  location: string;
+  contact: string;
+}
+
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent {
+
+  
 
 }
